1f3a2c81db036b4a0e97ab65d02544c294bc7a86
TrueAlexCv <31777187+TrueAlexCv@users.noreply.github.com>
2021-01-23 22:30:41 +0100

    Frontend

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -9,9 +9,67 @@ import { Publication } from '../models/publication';
 
 export class UserService {
     public url: string;
+    public identity;
+    public token;
+    public stats;
     
     constructor(public _http: HttpClient) {
         this.url = GLOBAL.url;
+    }
+    
+    registerUser(user: User): Observable<any> {
+        let params = JSON.stringify(user);
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        
+        return this._http.post(this.url + 'register', params, 
+                {headers: headers});
+    }
+    
+    loginUser(user: User, gettoken=null): Observable<any> {
+        if(gettoken != null) {
+            user = Object.assign(user, {gettoken});
+        }
+        
+        let params = JSON.stringify(user);
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        
+        return this._http.post(this.url + 'login', params, {headers: headers});
+    }
+    
+    getIdentity() {
+        let identity = JSON.parse(localStorage.getItem('identity'));
+        
+        if(identity != 'undefined') {
+            this.identity = identity;
+        } else {
+            this.identity = null;
+        }
+        
+        return this.identity;
+    }
+    
+    getToken() {
+        let token = localStorage.getItem('token');
+        
+        if(token != 'undefined') {
+            this.token = token
+        } else {
+            this.token = null;
+        }
+        
+        return this.token;
+    }
+    
+    getStats() {
+        let stats = JSON.parse(localStorage.getItem('stats'));
+        
+        if(stats != 'undefined') {
+            this.stats = stats;
+        } else {
+            this.stats = null;
+        }
+        
+        return this.stats;
     }
 }
 

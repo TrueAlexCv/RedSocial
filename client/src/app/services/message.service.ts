35fff16cb181c8832c590441abda681c6c08d4d8
TrueAlexCv <alexgamestrue2428@gmail.com>
2021-02-09 20:51:45 +0100

    Modificaciones

---
+++ client/src/app/services/message.service.ts
@@ -0,0 +1,54 @@
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {GLOBAL} from './global';
+
+@Injectable()
+
+export class MessageService {
+  public url: string;
+
+  constructor(private http: HttpClient) {
+    this.url = GLOBAL.url;
+  }
+
+  addMessage(token: any, message: any): Observable<any> {
+    const params = JSON.stringify(message);
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+      .set('Authorization', token);
+
+    return this.http.post(this.url + 'add-message', params, {headers: headers});
+  }
+
+  getMessages(token: any, userId: any): Observable<any> {
+    const params = JSON.stringify(userId);
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+      .set('Authorization', token);
+
+    return this.http.post(this.url + 'get-messages', params, {headers: headers});
+  }
+
+  getEmittedMessages(token: any, userId: any): Observable<any> {
+    const params = JSON.stringify(userId);
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+      .set('Authorization', token);
+
+    return this.http.post(this.url + 'get-emitted-messages', params, {headers: headers});
+  }
+
+  getReceivedMessages(token: any, userId: any): Observable<any> {
+    const params = JSON.stringify(userId);
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+      .set('Authorization', token);
+
+    return this.http.post(this.url + 'get-received-messages', params, {headers: headers});
+  }
+
+  setViewedMessages(token: any, message: any): Observable<any> {
+    const params = JSON.stringify(message);
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+      .set('Authorization', token);
+
+    return this.http.post(this.url + 'set-viewed-messages', params, {headers: headers});
+  }
+}
